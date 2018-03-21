c6d213891577290d056bbfa98120af10b655e77e
luke <yinluke@gmail.com>
2018-03-21 15:13:58 -0400

    initial
    initial commit

---
+++ EazyFitness/Alphanumeric.swift
@@ -0,0 +1,105 @@
+//
+//  Alphanumeric.swift
+//  EazyFitness
+//
+//  Created by Luke on 2018-03-15.
+//  Copyright © 2018 luke. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class Alphanumeric: NSObject {
+    static let literal = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    class func convert(Input:Int) -> String{
+        if (Input<0){
+            return "NULL"
+        }
+        if(Input>=62){
+            return Alphanumeric.convert(Input: Int(floor(Double(Input/62))))+Alphanumeric.convert(Input: Input%62)
+        } else {
+            return String(literal[Input])
+        }
+    }
+    class func convert(Input:String) -> Int{
+        return Alphanumeric.convert(Input: Input, remainder: 0)
+    }
+    class func convert(Input:String, remainder:Int) -> Int{
+        if Input.isAlphanumeric{
+            if Input.count > 1{
+                return Alphanumeric.convert(Input: String(Input[1...]), remainder:literal.indexDistance(of: Input[0])! + remainder*62)
+            } else if Input.count == 1{
+                return literal.indexDistance(of: Input[0])! + remainder*62
+            } else {
+                return remainder
+            }
+        }
+        return 0
+    }
+}
+
+extension String {
+    subscript (i: Int) -> Character {
+        return self[index(startIndex, offsetBy: i)]
+    }
+    subscript (bounds: CountableRange<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return self[start ..< end]
+    }
+    subscript (bounds: CountableClosedRange<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return self[start ... end]
+    }
+    subscript (bounds: CountablePartialRangeFrom<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(endIndex, offsetBy: -1)
+        return self[start ... end]
+    }
+    subscript (bounds: PartialRangeThrough<Int>) -> Substring {
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return self[startIndex ... end]
+    }
+    subscript (bounds: PartialRangeUpTo<Int>) -> Substring {
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return self[startIndex ..< end]
+    }
+    var isAlphanumeric: Bool {
+        return !isEmpty && range(of: "[^a-zA-Z0-9]", options: .regularExpression) == nil
+    }
+    func indexDistance(of character: Character) -> Int? {
+        guard let index = index(of: character) else { return nil }
+        return distance(from: startIndex, to: index)
+    }
+}
+
+extension Substring {
+    subscript (i: Int) -> Character {
+        return self[index(startIndex, offsetBy: i)]
+    }
+    subscript (bounds: CountableRange<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return self[start ..< end]
+    }
+    subscript (bounds: CountableClosedRange<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return self[start ... end]
+    }
+    subscript (bounds: CountablePartialRangeFrom<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(endIndex, offsetBy: -1)
+        return self[start ... end]
+    }
+    subscript (bounds: PartialRangeThrough<Int>) -> Substring {
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return self[startIndex ... end]
+    }
+    subscript (bounds: PartialRangeUpTo<Int>) -> Substring {
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return self[startIndex ..< end]
+    }
+}
+
