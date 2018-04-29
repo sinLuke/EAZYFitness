fb2d5f8491604b236138db84959d88a1ea09796a
evo <linboyy@hotmail.com>
2018-04-29 19:15:52 -0400

    Mystudent(AllStudent)

--- EazyFitness/ViewController/MyStudentVC.swift
+++ EazyFitness/ViewController/MyStudentVC.swift
@@ -7,15 +7,50 @@
 //
 
 import UIKit
+import Firebase
 
-class MyStudentVC: DefaultViewController {
+
+class MyStudentVC: DefaultViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    var dic = NSDictionary()
+    var sortedKeys = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
+    
 
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dic.count
+    }
+    
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        sortedKeys = [String](dic.allKeys as! [String]).sorted() // +++++++++++++++++++++++++++
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "allStudentCell")
+        let rowNum = indexPath.row
+        let rowInfo = dic.value(forKey: sortedKeys[rowNum]) as? NSDictionary
+        
+        cell.textLabel?.text = "\(sortedKeys[rowNum])"
+        
+        if ((rowInfo?.value(forKey: "Registered") as? Int) != 1) {
+            cell.detailTextLabel?.text = "未注册"
+            cell.detailTextLabel?.textColor = UIColor.red
+            
+        } else {
+            let fname = rowInfo?.value(forKey: "First Name") ?? ""
+            let lname = rowInfo?.value(forKey: "Last Name") ?? String(sortedKeys[rowNum])
+         
+            cell.detailTextLabel?.text = "\(fname) \(lname)"
+        }
+        
+        return (cell)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
